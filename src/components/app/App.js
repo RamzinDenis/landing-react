1f5537d323927f07971e683b5e28f6261a55181a
RamzinDenis <denis.ramzin@mail.ru>
2020-05-26 20:42:22 +0300

    add slider-contents folder + homepage content

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import Header from "../header";
 import Main from "../main";
 import Slider from "../slider";
+import HomePageContent from "../slider/slider-content";
 
 const App = () => {
 	return (
 		<>
 			<Header />
-			<Slider />
+			<Slider children={HomePageContent} />
 			<Main />
 		</>
 	);
---
+++ src/components/slider/slider-content/homepage-content/HomePageContent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./homepage-content.module.scss";
+
+const HomePageSliderItem = () => {
+	return (
+		<div className={styles.homepage_content}>
+			<h2 className={styles.homepage_slider__title}></h2>
+			<button></button>
+		</div>
+	);
+};
+
+export default HomePageSliderItem;
