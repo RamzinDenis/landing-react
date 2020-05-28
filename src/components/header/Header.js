d73149b0b81ca07d65760e244b97bc4f6c46be3d
RamzinDenis <denis.ramzin@mail.ru>
2020-05-28 13:15:31 +0300

    finish adaptive header-menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./header.module.scss";
 import NavBar from "../nav-bar";
 import Logo from "../logo";
 import HeaderInfo from "./header-info";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+	const [visible, setVisible] = useState(false);
 	return (
 		<header className={styles.header}>
 			<div className={styles.header__container}>
 				<Logo />
-				<div>
+				<div className={styles.header__menu}>
 					<HeaderInfo />
 					<NavBar classN={styles.header__navbar} />
 				</div>
 			</div>
+			<FontAwesomeIcon
+				icon={faBars}
+				className={styles.icon}
+				onClick={() => setVisible(!visible)}
+			/>
+			{visible && (
+				<div className={styles.mobile__menu}>
+					<NavBar classN={styles.header__navbar} />
+				</div>
+			)}
 		</header>
 	);
 };
